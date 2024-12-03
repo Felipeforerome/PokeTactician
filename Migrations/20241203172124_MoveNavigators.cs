b3aebe2a61bb368f6124bf0dd1b14ad43be48b46
Felipe Forero Meola <felipe.forerome@gmail.com>
2024-12-03 18:22:49 +0100

    Add MoveNavigators migration to update foreign key relationships for Pokemons

---
+++ Migrations/20241203172124_MoveNavigators.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokeTactician_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoveNavigators : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pokemons_Types_Type1Id",
+                table: "Pokemons");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pokemons_Types_Type2Id",
+                table: "Pokemons");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pokemons_Types_Type1Id",
+                table: "Pokemons",
+                column: "Type1Id",
+                principalTable: "Types",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pokemons_Types_Type2Id",
+                table: "Pokemons",
+                column: "Type2Id",
+                principalTable: "Types",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pokemons_Types_Type1Id",
+                table: "Pokemons");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pokemons_Types_Type2Id",
+                table: "Pokemons");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pokemons_Types_Type1Id",
+                table: "Pokemons",
+                column: "Type1Id",
+                principalTable: "Types",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pokemons_Types_Type2Id",
+                table: "Pokemons",
+                column: "Type2Id",
+                principalTable: "Types",
+                principalColumn: "Id");
+        }
+    }
+}
