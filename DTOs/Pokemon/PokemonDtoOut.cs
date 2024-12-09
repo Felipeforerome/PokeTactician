4879df55aa0cf2ee3970f29813a4b13617c5f882
Felipe Forero Meola <felipe.forerome@gmail.com>
2024-12-09 23:26:30 +0100

    Types, Games and Moves as strings in the DTO Outs fro readability

--- DTOs/Pokemon/PokemonDtoOut.cs
+++ DTOs/Pokemon/PokemonDtoOut.cs
@@ -9,12 +9,12 @@ public class PokemonDtoOut
     public int SpAtt { get; set; }
     public int SpDeff { get; set; }
     public int Spe { get; set; }
-    public required PokemonTypeDtoOut Type1 { get; set; }
-    public PokemonTypeDtoOut? Type2 { get; set; }
+    public required string Type1 { get; set; }
+    public string? Type2 { get; set; }
     public bool Mythical { get; set; }
     public bool Legendary { get; set; }
     public bool BattleOnly { get; set; }
     public bool Mega { get; set; }
-    public required List<int> KnowableMoves { get; set; } = new List<int>();
-    public required List<int> Games { get; set; } = new List<int>();
+    public required List<string> KnowableMoves { get; set; } = new List<string>();
+    public required List<string> Games { get; set; } = new List<string>();
 }
--- Mappings/MappingProfile.cs
+++ Mappings/MappingProfile.cs
@@ -10,10 +10,12 @@ namespace PokeTactician_Backend.Mappings
         {
             CreateMap<PokemonDtoIn, Pokemon>();
             CreateMap<Pokemon, PokemonDtoOut>()
-                .ForMember(dest => dest.KnowableMoves, opt => opt.MapFrom(src => src.KnowableMoves.Select(m => m.Id)))
-                .ForMember(dest => dest.Games, opt => opt.MapFrom(src => src.Games.Select(g => g.Id)));
+                .ForMember(dest => dest.Type1, opt => opt.MapFrom(src => src.Type1.Name))
+                .ForMember(dest => dest.Type2, opt => opt.MapFrom(src => src.Type2 != null ? src.Type2.Name : null))
+                .ForMember(dest => dest.KnowableMoves, opt => opt.MapFrom(src => src.KnowableMoves.Select(m => m.Name)))
+                .ForMember(dest => dest.Games, opt => opt.MapFrom(src => src.Games.Select(g => g.Name)));
             CreateMap<Move, MoveDtoOut>()
-                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type));
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.Name));
             CreateMap<MoveDtoIn, Move>();
             CreateMap<PokemonType, PokemonTypeDtoOut>();
             CreateMap<PokemonTypeDtoIn, PokemonType>();
