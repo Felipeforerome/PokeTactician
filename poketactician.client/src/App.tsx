0972dccfc2f472beb2fe915290a0842972250d06
Felipe Forero Meola <felipe.forerome@gmail.com>
2024-12-17 23:38:40 +0100

    Cards with pop-out image, grids and bgGradient

--- poketactician.client/src/App.tsx
+++ poketactician.client/src/App.tsx
@@ -1,7 +1,18 @@
 import { useEffect, useState } from 'react';
+import {
+    Drawer,
+    DrawerContent,
+    DrawerHeader,
+    DrawerBody,
+    DrawerFooter,
+    useDisclosure,
+    Button
+} from "@nextui-org/react";
 import './App.css';
+import PokemonCard from './components/PokemonCard/PokemonCard';
 
 interface Pokemon {
+    id: number;
     name: string;
     hp: number;
     att: number;
@@ -14,50 +25,76 @@ interface Pokemon {
 }
 
 function App() {
-    const [forecasts, setPokemons] = useState<Pokemon[]>();
+    const [pokemons, setPokemons] = useState<Pokemon[]>();
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
     useEffect(() => {
         populatePokemonData();
     }, []);
 
-    const contents = forecasts === undefined
+    const contents = pokemons === undefined
         ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>HP</th>
-                    <th>Attack</th>
-                    <th>Defense</th>
-                    <th>Special Attack</th>
-                    <th>Special Defense</th>
-                    <th>Speed</th>
-                    <th>Type 1</th>
-                    <th>Type 2</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.name}>
-                        <td>{forecast.name}</td>
-                        <td>{forecast.hp}</td>
-                        <td>{forecast.att}</td>
-                        <td>{forecast.deff}</td>
-                        <td>{forecast.spAtt}</td>
-                        <td>{forecast.spDeff}</td>
-                        <td>{forecast.spe}</td>
-                        <td>{forecast.type1}</td>
-                        <td>{forecast.type2}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
+        : <div className='grid grid-cols-1 md:grid-cols-3  gap-4'>
+            {pokemons.map((pokemon, index) => (
+                <div className='col-span-1' key={index}>
+                    <PokemonCard
+                        name={pokemon.name}
+                        image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                        type1={pokemon.type1}
+                        type2={pokemon.type2}
+                        hp={pokemon.hp}
+                        attack={pokemon.att}
+                        defense={pokemon.deff}
+                    />
+                </div>
+            ))}
+        </div>
 
     return (
         <div>
             <h1 id="tableLabel">Pokemon Stats</h1>
             <p>This component demonstrates fetching data from the server.</p>
             {contents}
+            <br />
+            <>
+                <Button className='dark' onPress={onOpen}>Open Drawer</Button>
+                <Drawer isOpen={isOpen} onOpenChange={onOpenChange} className='dark'>
+                    <DrawerContent>
+                        {(onClose) => (
+                            <>
+                                <DrawerHeader className="flex flex-col gap-1">Drawer Title</DrawerHeader>
+                                <DrawerBody>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
+                                        risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
+                                        quam.
+                                    </p>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
+                                        risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
+                                        quam.
+                                    </p>
+                                    <p>
+                                        Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor
+                                        adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit
+                                        officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt
+                                        nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa
+                                        deserunt nostrud ad veniam.
+                                    </p>
+                                </DrawerBody>
+                                <DrawerFooter>
+                                    <Button color="danger" variant="light" onPress={onClose}>
+                                        Close
+                                    </Button>
+                                    <Button color="primary" onPress={onClose}>
+                                        Action
+                                    </Button>
+                                </DrawerFooter>
+                            </>
+                        )}
+                    </DrawerContent>
+                </Drawer>
+            </>
         </div>
     );
 
