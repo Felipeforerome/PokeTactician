0f98ac2693d90f2cd6133a523079ec2fe9a6a943
Felipe Forero Meola <felipe.forerome@gmail.com>
2024-12-28 00:40:48 +0100

    PokemonDtoName has an id

--- PokeTactician.Server/DTOs/Pokemon/PokemonDtoName.cs
+++ PokeTactician.Server/DTOs/Pokemon/PokemonDtoName.cs
@@ -2,6 +2,7 @@ namespace PokeTactician.DTOs
 {
     public class PokemonDtoName
     {
+        public int Id { get; set; }
         public required string Name { get; set; } // Replace 'Name' with the field you are interested in
     }
 }
