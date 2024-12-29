43f77c52cdbc7b4a822b1c15f9e78f3156b77717
Felipe Forero Meola <felipe.forerome@gmail.com>
2024-12-29 20:32:21 +0100

    Fixed grid logic, based on screen size

--- poketactician.client/src/components/PreselectTeam.tsx
+++ poketactician.client/src/components/PreselectTeam.tsx
@@ -6,7 +6,11 @@ import PokemonSelector from './PokemonSelector';
 
 export function PreselectTeam() {
   const [team, setTeam] = useState<Pokemon[]>([]);
-
+  let gridClass = `grid grid-cols-1 ${
+    team.length > 0 ? 'md:grid-cols-2' : 'md:grid-cols-1'
+  } ${
+    team.length > 0 ? 'xl:grid-cols-3' : 'xl:grid-cols-1'
+  } gap-4 overflow-visible justify-items-center`;
   async function handleAddtoTeam(pokemonParam: any) {
     const pokemon = pokemonParam[0];
     let pokemonMember = await getPokemon(pokemon[0]);
@@ -25,24 +29,29 @@ export function PreselectTeam() {
           Preselect your team
         </h2>
         <div className="flex-grow flex items-center -mt-[55px] md:-mt-[85px]">
-          <div
-            className={`grid grid-cols-1 md:grid-cols-${Math.min(
-              team.length,
-              2,
-            )} lg:grid-cols-${Math.min(
-              team.length,
-              3,
-            )} gap-4 overflow-visible justify-items-center`}
-          >
-            <PokemonSelector addPokemon={handleAddtoTeam} />
+          <div className={gridClass}>
             <AnimatePresence>
               {team.map((pokemon, index) => (
                 <PokemonCard key={index} {...pokemon} />
               ))}
+              {team.length < 6 ? (
+                <PokemonSelector addPokemon={handleAddtoTeam} />
+              ) : null}
             </AnimatePresence>
           </div>
         </div>
       </div>
+      <p className="text-center mt-4">
+        {window.innerWidth >= 1280
+          ? 'You are at the xl breakpoint'
+          : window.innerWidth >= 1024
+          ? 'You are at the lg breakpoint'
+          : window.innerWidth >= 768
+          ? 'You are at the md breakpoint'
+          : window.innerWidth >= 640
+          ? 'You are at the sm breakpoint'
+          : 'You are at the base breakpoint'}
+      </p>
       {/* <div>
         {team.map((pokemon) => (
           <div key={pokemon.id} onClick={() => handleClick(pokemon)}>
