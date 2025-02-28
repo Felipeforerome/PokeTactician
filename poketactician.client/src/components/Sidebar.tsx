9150a88a29aeee44b80b0e4e92eb341641beb130
Felipe Forero Meola <felipe.forerome@gmail.com>
2025-03-01 00:59:08 +0100

    Add strategy and role selection to Sidebar component

--- poketactician.client/src/components/Sidebar.tsx
+++ poketactician.client/src/components/Sidebar.tsx
@@ -2,12 +2,14 @@
 import Filters from './Filters';
 import { FiltersProps } from './Filters';
 import StrategyRoles from './StrategyRoles';
+import { useState } from 'react';
 
 export default function Sidebar({ updateFilters, applyFilters }: FiltersProps) {
   const handleFilterChange = (id: string, value: any) => {
     updateFilters(id, value);
   };
-
+  const [strategy, setStrategy] = useState<string>('none');
+  const [roles, setRoles] = useState<string[]>([]);
   const handleApply = () => {
     // Put your filter application logic here
     applyFilters();
@@ -16,7 +18,10 @@ export default function Sidebar({ updateFilters, applyFilters }: FiltersProps) {
   return (
     <aside className="flex flex-col bg-background-500 justify-center w-[250px] min-w-[250px] h-[calc(100vh-64px)] p-4 text-white gap-4 fixed top-16 left-0">
       <Filters updateFilters={handleFilterChange} applyFilters={handleApply} />
-      <StrategyRoles />
+      <StrategyRoles
+        handleStrategyChange={setStrategy}
+        handleRoleChange={setRoles}
+      />
     </aside>
   );
 }
--- poketactician.client/src/components/StrategyRoles.tsx
+++ poketactician.client/src/components/StrategyRoles.tsx
@@ -1,13 +1,67 @@
-import { Accordion, AccordionItem } from "@heroui/react";
+import {
+  Listbox,
+  ListboxItem,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Button,
+  Selection,
+  Checkbox,
+  CheckboxGroup,
+} from '@heroui/react';
+import { roles } from '../data/roles';
+import { strategies } from '../data/strategies';
+interface StrategyRolesProps {
+  handleStrategyChange: (key: string) => void;
+  handleRoleChange: (roles: string[]) => void;
+}
 
-export default function StrategyRoles() {
+export default function StrategyRoles({
+  handleStrategyChange,
+  handleRoleChange,
+}: StrategyRolesProps) {
+  const defaultKey = new Set([strategies[0].key]);
+
+  const onStrategySelect = (key: string) => {
+    handleStrategyChange(key);
+  };
   return (
     <div>
-      <Accordion>
-        <AccordionItem title="Strategy and Roles">
-          <p>Hello</p>
-        </AccordionItem>
-      </Accordion>
+      <Popover placement="right">
+        <PopoverTrigger>
+          <Button fullWidth>Strategy</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <Listbox
+            disallowEmptySelection
+            defaultSelectedKeys={defaultKey}
+            selectionMode="single"
+            aria-label="Strategies"
+            onSelectionChange={(key: Selection) =>
+              onStrategySelect(Array.from(key)[0] as string)
+            }
+          >
+            {strategies.map((strat) => (
+              <ListboxItem key={strat.key}>{strat.text}</ListboxItem>
+            ))}
+          </Listbox>
+        </PopoverContent>
+      </Popover>
+      <div className="p-1.5"></div>
+      <Popover placement="right-end">
+        <PopoverTrigger>
+          <Button fullWidth>Roles</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <CheckboxGroup className="p-2" onValueChange={handleRoleChange}>
+            {roles.map((role) => (
+              <Checkbox key={role.value} value={role.value}>
+                {role.text}
+              </Checkbox>
+            ))}
+          </CheckboxGroup>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
