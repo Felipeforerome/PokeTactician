41fde9abda443707b58e5745e81d80b2817c6f4e
Felipe Forero Meola <felipe.forerome@gmail.com>
2025-03-15 01:46:19 +0100

    Fix casing of Damage_Class property to Damage_class in MoveDtoOut

--- PokeTactician.Server/DTOs/Move/MoveDtoOut.cs
+++ PokeTactician.Server/DTOs/Move/MoveDtoOut.cs
@@ -8,6 +8,6 @@ public class MoveDtoOut
     public int Power { get; set; }
     public float Accuracy { get; set; }
     public int PP { get; set; }
-    public required string Damage_Class { get; set; }
+    public required string Damage_class { get; set; }
     public int Priority { get; set; }
 }
