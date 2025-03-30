703a86721c6f81d8bfcaa3ff8ba432dbbd66075c
Felipe Forero Meola <felipe.forerome@gmail.com>
2025-03-30 22:50:07 +0200

    Update TeamDashboard component header and add console log for team

--- poketactician.client/src/components/TeamDashboard.tsx
+++ poketactician.client/src/components/TeamDashboard.tsx
@@ -3,9 +3,10 @@ interface TeamDashboardProps {
   team: Pokemon[];
 }
 export function TeamDashboard({ team }: TeamDashboardProps) {
+  console.log(team);
   return (
     <div className="flex justify-center items-center h-full">
-      <h1>Other Side</h1>
+      <h1>Still in construction</h1>
     </div>
   );
 }
