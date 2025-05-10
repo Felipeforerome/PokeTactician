5e2df6be13dee8db31e4e7788b6d73620122dba4
Felipe Forero Meola <felipe.forerome@gmail.com>
2025-05-10 14:55:13 +0200

    Meta tag to avoid zooming on forms

--- poketactician.client/src/main.tsx
+++ poketactician.client/src/main.tsx
@@ -6,6 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
     <HeroUIProvider>
       <App />
     </HeroUIProvider>
